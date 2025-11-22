05d46532ab7db862d9fe6b11f7e75b92fbf11b9f
markcoders1 <developer@markcoders.com>
2025-11-22 05:36:40 +0500

    model

--- models/lead.model.js
+++ models/lead.model.js
@@ -2,23 +2,50 @@ const mongoose = require("mongoose");
 
 const leadSchema = new mongoose.Schema(
     {
-        name: {
+        Name: {
             type: String,
             required: [true, "Name is required"],
             trim: true,
         },
-        email: {
+        Email: {
             type: String,
             required: [true, "Email is required"],
             trim: true,
             lowercase: true,
         },
-        phone: {
+        Address: {
             type: String,
+            required: [true, "Address is required"],
             trim: true,
         },
-        message: {
+        Phone: {
             type: String,
+            required: [true, "Phone is required"],
+            trim: true,
+        },
+        Service: {
+            type: String,
+            required: [true, "Service is required"],
+            trim: true,
+        },
+        Timeline: {
+            type: String,
+            required: [true, "Timeline is required"],
+            trim: true,
+        },
+        ProjectDetails: {
+            type: String,
+            required: [true, "ProjectDetails is required"],
+            trim: true,
+        },
+        purpose: {
+            type: String,
+            required: [true, "purpose is required"],
+            trim: true,
+        },
+        ref: {
+            type: String,
+            required: [true, "ref is required"],
             trim: true,
         },
     },
